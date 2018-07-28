d17835c2bebcf5841cff9189c898b842af733e1a
gitMacchina <the.richard.cooke@gmail.com>
2018-07-28 16:24:59 +0100

    enquire pop up works

---
+++ awh_build/home/static/home/js/enquire.js
@@ -0,0 +1,8 @@
+function toggle_form() {
+    var pop_up = document.getElementById("popup_jumbo");
+    if (pop_up.style.visibility == "hidden") {
+        pop_up.style.visibility = "visible";
+    } else {
+        pop_up.style.visibility = "hidden";
+    }
+}
