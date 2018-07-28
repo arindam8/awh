50b90537b9d9dbeddb155655be61c9427220c25a
gitMacchina <the.richard.cooke@gmail.com>
2018-07-28 17:07:13 +0100

    button animation done
    form animation to do

--- awh_build/home/static/home/js/enquire.js
+++ awh_build/home/static/home/js/enquire.js
@@ -2,7 +2,30 @@ function toggle_form() {
     var pop_up = document.getElementById("popup_jumbo");
     if (pop_up.style.visibility == "hidden") {
         pop_up.style.visibility = "visible";
+        fadeOut()
     } else {
         pop_up.style.visibility = "hidden";
+        fadein()
     }
+}
+
+function fadeOut() {
+    var fadeTarget = document.getElementById("enquire_widget");
+    fadeTarget.classList.add('playstate');
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 50);
+}
+
+function fadein() {
+    var fadeTarget = document.getElementById("enquire_widget");
+    fadeTarget.classList.remove('playstate');
+    fadeTarget.style.opacity = 1
 }
