818de859312813be7e93e0037defd2f03a0f04ea
gitMacchina <the.richard.cooke@gmail.com>
2018-07-29 16:03:48 +0100

    lots of refinement

--- awh_build/home/static/home/js/enquire.js
+++ awh_build/home/static/home/js/enquire.js
@@ -1,5 +1,6 @@
 var pop_up = document.getElementById("popup_jumbo");
 var fadeTarget = document.getElementById("enquire_widget");
+var fadeTarget2 = document.getElementById("enquire_widget_mini");
 var toggle = 1;
 
 function toggle_form() {
@@ -17,6 +18,8 @@ function toggle_form() {
 function fadeOut() {
     fadeTarget.classList.add('playstate');
     fadeTarget.classList.add('enquire_widget_translate');
+    fadeTarget2.classList.add('playstate');
+    fadeTarget2.classList.add('enquire_widget_translate');
 }
 
 
@@ -31,4 +34,6 @@ function slideOut() {
 function fadein() {
     fadeTarget.classList.remove('playstate');
     fadeTarget.classList.remove('enquire_widget_translate');
+    fadeTarget2.classList.remove('playstate');
+    fadeTarget2.classList.remove('enquire_widget_translate');
 }
