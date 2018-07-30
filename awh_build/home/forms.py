316093d3f01cd803d191fc99e50072c5b48574be
ec15275 <ec15275@se15.qmul.ac.uk>
2018-07-30 19:55:32 +0100

    Updated form css

--- awh_build/home/forms.py
+++ awh_build/home/forms.py
@@ -11,10 +11,10 @@ class EnquiryForm(forms.Form):
 
     )
 
-    name=forms.CharField(label="Name",max_length=1000,widget = forms.TextInput(attrs={'id':'name','class': "form-control"}))
-    email = forms.EmailField(label="Email",widget = forms.TextInput(attrs={'id':'email','class': "form-control"}))
-    description = forms.CharField(label="Description",max_length=1000,widget = forms.Textarea(attrs={'id':'comment','rows':'1','class': "form-control"}))
-    postcode = forms.CharField(label="Postcode",max_length=100,widget = forms.TextInput(attrs={'id':'postcode','class': "form-control"}))
-    attach = forms.Field(label="Attachment",widget = forms.FileInput(attrs={'class': "form-control"}))
-    phone = forms.CharField(label="Phone",widget = forms.TextInput(attrs={'id':'number','class': "form-control"}))
-    category=forms.ChoiceField(label="Category",choices=CHOICES,widget = forms.RadioSelect(attrs={'class': "form-control enquire_radio_circle"}))
+    name=forms.CharField(label="Name",max_length=1000,widget = forms.TextInput(attrs={'placeholder':'Name','id':'name','class': "form-control"}))
+    email = forms.EmailField(label="Email",widget = forms.TextInput(attrs={'placeholder':'E-mail','id':'email','class': "form-control"}))
+    description = forms.CharField(label="Description",max_length=1000,widget = forms.Textarea(attrs={'placeholder':'Description','id':'comment','rows':'1','class': "form-control"}))
+    postcode = forms.CharField(label="Postcode",max_length=100,widget = forms.TextInput(attrs={'placeholder':'Postcode','id':'postcode','class': "form-control"}))
+    attach = forms.Field(label="Attachment",widget = forms.FileInput(attrs={'placeholder':'Attachment','class': "form-control"}))
+    phone = forms.CharField(label="Phone",widget = forms.TextInput(attrs={'placeholder':'Phone Number','id':'number','class': "form-control"}))
+    category=forms.ChoiceField(label="Category",choices=CHOICES,widget = forms.RadioSelect(attrs={'placeholder':'Category','class': "form-control enquire_radio_circle"}))
