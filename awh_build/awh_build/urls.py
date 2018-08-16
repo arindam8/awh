c524bf293512f091694409303c0238678c67169e
gitMacchina <the.richard.cooke@gmail.com>
2018-08-16 21:31:14 +0100

    commit images

--- awh_build/awh_build/urls.py
+++ awh_build/awh_build/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from home import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     #url(r'^home/', include('home.urls'))
