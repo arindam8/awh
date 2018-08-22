684bb9d2cec69223ed66c38cbc98e72267409c3a
gitMacchina <the.richard.cooke@gmail.com>
2018-08-22 09:33:57 +0100

    styled it up

--- awh_build/home/static/home/js/tweet.js
+++ awh_build/home/static/home/js/tweet.js
@@ -7,6 +7,7 @@ for (let i = 0; i < tweets.length; i++) {
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.news_date').forEach((date, i) => {date.innerHTML = tweets[i].created})
+    document.querySelectorAll('.news_date').forEach((date, i) => {date.setAttribute('href', tweets[i].link_)})
     document.querySelectorAll('.news_content').forEach((content, i) => {content.innerHTML = tweets[i].text})
     document.querySelectorAll('.news_link').forEach((link, i) => {link.setAttribute('href', tweets[i].link_)})
 })
